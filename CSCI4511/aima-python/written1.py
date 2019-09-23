ad45bdfeaee713e1a60882b412cd89e5470aa038
Zachary Riehl <riehl046@umn.edu>
2019-09-22 19:10:29 -0500

    updating written1.py

--- CSCI4511/aima-python/written1.py
+++ CSCI4511/aima-python/written1.py
@@ -14,3 +14,12 @@ def p3():
     depth_first_tree_search(EightPuzzle((6,2,3,1,5,4,0,7,8)))
 
 p3()
+
+def p4():
+    depth_first_tree_search(NQueensProblem(8))
+
+def p5():
+    print("BFS Tree")
+    breadth_first_tree_search(NQueensProblem(8))
+
+p5()
