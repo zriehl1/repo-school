c583ff8485e2e8259be28033a99cb3f026b6c9c6
Zachary Riehl <riehl046@umn.edu>
2019-09-24 23:02:20 -0500

    getting memory data for nqueen

--- CSCI4511/aima-python/written1.py
+++ CSCI4511/aima-python/written1.py
@@ -1,5 +1,5 @@
 from search import *
-
+SIZE = 8
 def p1():
     breadth_first_tree_search(EightPuzzle((6,2,3,1,5,4,0,7,8)))
     while True:
@@ -12,14 +12,17 @@ def p2():
 
 def p3():
     depth_first_tree_search(EightPuzzle((6,2,3,1,5,4,0,7,8)))
-
-p3()
-
+    
 def p4():
-    depth_first_tree_search(NQueensProblem(8))
+    print("DFS Tree " + str(SIZE))
+    depth_first_tree_search(NQueensProblem(SIZE))
+    while True:
+        None
 
 def p5():
-    print("BFS Tree")
-    breadth_first_tree_search(NQueensProblem(8))
+    print("BFS Tree " + str(SIZE))
+    breadth_first_tree_search(NQueensProblem(SIZE))
+    while True:
+        None
 
-p5()
+p4()
